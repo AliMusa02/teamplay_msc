1e1e27cd3852c6f2f7b071503a23a70ad2e68811
AliMusa02 <ali.musayev044@gmail.com>
2025-07-02 17:50:50 +0000

    frontend setted up, routes added, protected route component created, pages setup created

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom"
+import api from "../api"
+import { act, useState } from "react"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+
+
+
+function Login() {
+
+    // function Form({ route, method }) {
+    //     const [email, setEmail] = useState("")
+    //     const [password, setPassword] = useState("")
+    //     const [loading, setLoading] = useState(false)
+    //     const Navigate = useNavigate()
+
+
+    // }
+    return <div>Login page</div>
+
+}
+
+export default Login
